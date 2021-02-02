f0928a4ff24af6a6730343f16356217a56087995
ajitpsakri <smartsakri@gmail.com>
2021-02-02 18:10:11 +0530

    some heading and link to source code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Whitebutton from "./Components/Whitebutton";
 function App() {
   return (
     <div className="App">
-      <Whitebutton />
+      <h1>Click on a random button to make piano work</h1>
+      <div className="links">
+        <a href={"https://github.com/ajitpsakri/piyano"}>
+          Source-Code(I'm a newbie so it does have a lot of if-else ladder.)
+        </a>
+      </div>
+      <Whitebutton class="pyanos" />
     </div>
   );
 }
